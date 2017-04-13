d198a9d4f6922e344b1e7cf3774c8dfd99f7e98d
RandolphLIN <2933089932@qq.com>
2017-04-14 04:33:42 +0800

    411

--- js/default.js
+++ js/default.js
@@ -15,7 +15,7 @@ img1.addEventListener('mouseup', function(event){
 document.getElementById('img1').src="img/s2_btn1.png";
 console.log('up');
 
-window.location.assign("http://localhost/github/skatepark/GP/index.html");
+window.location.assign("index.html");
 // var btnX = document.getElementById('scene2').offsetLeft;
 // console.log(btnX);
 },false);
@@ -31,7 +31,7 @@ img1.addEventListener('touchend', function(event){
 document.getElementById('img1').src="img/s2_btn1.png";
 console.log('up');
 
-window.location.assign("http://localhost/github/skatepark/GP/index.html");
+window.location.assign("index.html");
 // var btnX = document.getElementById('scene2').offsetLeft;
 // console.log(btnX);
 },false);
@@ -52,7 +52,7 @@ img1.addEventListener('mouseup', function(event){
 document.getElementById('img2').src="img/s2_btn2.png";
 console.log('up');
 
-window.location.assign("http://localhost/github/skatepark/GP/index.html");
+window.location.assign("index.html");
 // var btnX = document.getElementById('img1').offsetLeft;
 // console.log(btnX);
 },false);
@@ -69,7 +69,7 @@ img1.addEventListener('touchend', function(event){
 document.getElementById('img2').src="img/s2_btn2.png";
 console.log('up');
 
-window.location.assign("http://localhost/github/skatepark/GP/index.html");
+window.location.assign("index.html");
 // var btnX = document.getElementById('img1').offsetLeft;
 // console.log(btnX);
 },false);
@@ -86,7 +86,7 @@ img1.addEventListener('mouseup', function(event){
 document.getElementById('img3').src="img/s2_btn3.png";
 console.log('up');
 
-window.location.assign("http://localhost/github/skatepark/GP/index.html");
+window.location.assign("index.html");
 },false);
 img1.addEventListener('mousedown', function(event){
 document.getElementById('img3').src="img/s2_btn1.png";
@@ -99,7 +99,7 @@ img1.addEventListener('touchend', function(event){
 document.getElementById('img3').src="img/s2_btn3.png";
 console.log('up');
 
-window.location.assign("http://localhost/github/skatepark/GP/index.html");
+window.location.assign("index.html");
 },false);
 img1.addEventListener('touchstart', function(event){
 document.getElementById('img3').src="img/s2_btn1.png";
