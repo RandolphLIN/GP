769b603f874aac7ff9144c48b96e991c6802a1e8
RandolphLIN <2933089932@qq.com>
2017-04-14 04:19:57 +0800

    414
    great progress of model

--- js/default.js
+++ js/default.js
@@ -10,6 +10,8 @@ var img1 =document.getElementById('img1');
 img1.addEventListener('mouseup', function(event){
 document.getElementById('img1').src="img/s2_btn1.png";
 console.log('up');
+
+window.location.assign("http://localhost/github/skatepark/GP/index.html");
 // var btnX = document.getElementById('scene2').offsetLeft;
 // console.log(btnX);
 },false);
@@ -25,6 +27,8 @@ var img1 =document.getElementById('img2');
 img1.addEventListener('mouseup', function(event){
 document.getElementById('img2').src="img/s2_btn2.png";
 console.log('up');
+
+window.location.assign("http://localhost/github/skatepark/GP/index.html");
 // var btnX = document.getElementById('img1').offsetLeft;
 // console.log(btnX);
 },false);
@@ -38,6 +42,8 @@ var img1 =document.getElementById('img3');
 img1.addEventListener('mouseup', function(event){
 document.getElementById('img3').src="img/s2_btn3.png";
 console.log('up');
+
+window.location.assign("http://localhost/github/skatepark/GP/index.html");
 },false);
 img1.addEventListener('mousedown', function(event){
 document.getElementById('img3').src="img/s2_btn1.png";
