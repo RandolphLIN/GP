2ca70923f2d1d25684e0e237edaaf17c62705bc0
RandolphLIN <2933089932@qq.com>
2017-05-09 16:50:56 +0800

    0509
    add information

--- js/default1.js
+++ js/default1.js
@@ -8,7 +8,7 @@ img1.addEventListener('mouseup', function(event){
 document.getElementById('start').src="img/s2_btn1.png";
 console.log('up');
 
-window.location.assign("default2.html");
+window.location.assign("frame.html");
 
 // var btnX = document.getElementById('scene2').offsetLeft;
 // console.log(btnX);
@@ -23,6 +23,8 @@ function y(){
 var img1 =document.getElementById('start');
 img1.addEventListener('touchend', function(event){
 document.getElementById('start').src="img/s2_btn1.png";
+
+window.location.assign("frame.html");
 },false);
 var img1 =document.getElementById('start');
 img1.addEventListener('touchstart', function(event){
